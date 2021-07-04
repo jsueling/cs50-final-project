1eb663ed610cf243b1320ebed558d6d88e5a126e
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-07-04 17:50:25 +0100

    added Config for session > signed cookies/syntax error and comment fixes

--- application.py
+++ application.py
@@ -1,26 +1,40 @@
 import os
 
 from flask import Flask, render_template, redirect, request, session
-
+from flask_session import Session
+from tempfile import mkdtemp
 from functions import error_page, login_required, lookup, usd
 
-app = FLask(__name__)
+# Setup the flask app
+app = Flask(__name__)
 
 # https://stackoverflow.com/questions/9508667/reload-flask-app-when-template-file-changes
-# Ensure templates are auto-reloaded
+# Ensure templates are auto-reloaded in the app when they are changed
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Ensure responses aren't cached after every API request
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Pragma
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expires
 # https://flask.palletsprojects.com/en/2.0.x/api/#flask.Flask.after_request
+# Ensure responses aren't cached in the headers of the response after every API request
+# Pragma is deprecated in HTTP 1.1 but used for backwards compatibility with HTTP 1.0
+# Setting value of 0 in expires means everything inside is now considered stale
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+# https://stackoverflow.com/questions/3948975/why-store-sessions-on-the-server-instead-of-inside-a-cookie
+# https://flask-session.readthedocs.io/en/latest/#configuration
+# Configure session to use filesystem (instead of signed cookies)
+# We make a temporary directory to store session files 
+# We configure session to be non-permanent (default true)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # https://flask.palletsprojects.com/en/2.0.x/templating/#registering-filters
 app.jinja_env.filters["usd"] = usd
